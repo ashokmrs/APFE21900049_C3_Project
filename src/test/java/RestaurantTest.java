4bc75317444e7c88bca5dec31fa328029de900db
ashokmrs <80301323+ashokmrs@users.noreply.github.com>
2021-09-10 00:20:19 +0530

    Update RestaurantTest.java

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -62,7 +62,7 @@ class RestaurantTest {
     }
     
     @Test
-    public void return_the_total_order_value_list_in_menu() throws itemNotFoundException {​
+    public void return_the_total_order_value_list_in_menu() throws itemNotFoundException {
         restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
@@ -75,7 +75,7 @@ class RestaurantTest {
         restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
         Item var = restaurant.getItemPrice("Sweet corn soup");
-        assertEquals(119,var.getItemPrice());
+        assertEquals(119,var.getPrice());
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 }
